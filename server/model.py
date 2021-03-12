d1e0cd7af5f84c1d6802f4b6178ee3444b2adca6
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2021-03-12 23:15:40 +0000

    Specify output path per model

--- server/model.py
+++ server/model.py
@@ -5,6 +5,7 @@ import pandas as pd
 from muse.mca import MCA
 import muse.examples
 from muse.investments import LinearProblemError
+from muse.readers.toml import read_settings
 
 from settings.config import MODEL_NAMES
 
@@ -17,15 +18,17 @@ class Model:
     def __init__(self):
         self.model: MCA
         self.path: Path
+        self.output_path: Path
         self.technodata: Dict[pd.DataFrame]
         self.agents: pd.DataFrame
 
     def select(self, name: str = "default"):
         self.path = muse.examples.copy_model(
             name=name,
-            path=Path("."),
+            path=Path().absolute() / "data" / name,
             overwrite=True,
         )
+        self.output_path = self.path.parent / "Results"
         self.load()
         self.technodata = {}
         for sec in self.sectors:
@@ -36,7 +39,22 @@ class Model:
         self.agents = pd.read_csv(self.path / "technodata" / "Agents.csv")
 
     def load(self):
-        self.model = MCA.factory(self.path / "settings.toml")
+        settings = read_settings(self.path / "settings.toml")
+
+        def prepend_path(outputs):
+            for index, output in enumerate(outputs):
+                relative_path = Path(output["filename"]).relative_to(Path().absolute())
+                outputs[index]["filename"] = self.path.parent / relative_path
+
+        prepend_path(settings.outputs)
+        for sector in settings.sectors:
+            if type(sector) == list:
+                continue
+            if "outputs" not in dir(sector):
+                continue
+            prepend_path(sector.outputs)
+
+        self.model = MCA.factory(settings)
 
     def run(self):
         for sec in self.sectors:
@@ -58,8 +76,8 @@ class Model:
 
     @property
     def capacity(self):
-        return pd.read_csv(Path("Results") / "MCACapacity.csv")
+        return pd.read_csv(self.output_path / "MCACapacity.csv")
 
     @property
     def prices(self):
-        return pd.read_csv(Path("Results") / "MCAPrices.csv")
+        return pd.read_csv(self.output_path / "MCAPrices.csv")
